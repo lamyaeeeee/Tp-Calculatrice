49d00fcde2c464da559970a5f50957e3a4d37873
rkay4779 <riyadh.kd10@gmail.com>
2024-01-01 21:41:21 +0100

    fixing the conflict

--- calculatrice/src/main/java/ma/ehei/calculatrice/App.java
+++ calculatrice/src/main/java/ma/ehei/calculatrice/App.java
@@ -4,5 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
 		System.out.println(Calculatrice.multiply(5, 5));
+		System.out.println(Calculatrice.add(8, 5));
 	}
 }
